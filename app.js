210e57e2cb9d724bc101fdaa4f2691c966f633e8
Tom Van Schoor <tom@tutuka.com>
2019-01-22 00:01:52 +0100

    Add config and logger
    The config can use ENV, ARGV and config.json

---
+++ app.js
@@ -0,0 +1,34 @@
+const neo4j = require('neo4j-driver').v1
+
+const logger = require('./logger/logger')
+const config = require('./config/config')
+
+logger.debug(config.get('neo4j_uri'))
+logger.debug(config.get('neo4j_user'))
+logger.debug(config.get('neo4j_password'))
+
+const driver = neo4j.driver(
+  config.get('neo4j_uri'),
+  neo4j.auth.basic(config.get('neo4j_user'), config.get('neo4j_password'))
+)
+
+const session = driver.session()
+
+const resultPromise = session.writeTransaction(tx =>
+  tx.run(
+    'CREATE (a:Greeting) SET a.message = $message RETURN a.message + ", from node " + id(a)',
+    { message: 'hello, world' }
+  )
+)
+
+resultPromise.then(result => {
+  session.close()
+
+  const singleRecord = result.records[0]
+  const greeting = singleRecord.get(0)
+
+  logger.info(greeting)
+
+  // on application exit:
+  driver.close()
+})
---
+++ logger/logger.js
@@ -0,0 +1,19 @@
+const { transports, createLogger, format } = require('winston')
+
+const config = require('../config/config')
+
+const logger = createLogger({
+  level: config.get('logger_level'),
+  format: format.combine(format.timestamp(), format.json()),
+  transports: [
+    new transports.Console({ timestamp: true }),
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/warn.log', level: 'warn' }),
+    new transports.File({ filename: 'logs/activity.log', level: 'info' }),
+    new transports.File({ filename: 'logs/verbose.log', level: 'verbose' }),
+    new transports.File({ filename: 'logs/debug.log', level: 'debug' }),
+    new transports.File({ filename: 'logs/silly.log', level: 'silly' })
+  ]
+})
+
+module.exports = logger
